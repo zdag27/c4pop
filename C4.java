baa2844ebd1c767d10f2698587fbb90629c4a7cc
TorchZz <63902825+TorchZz@users.noreply.github.com>
2021-11-12 15:48:53 +0100

    Add files via upload

--- C4.java
+++ C4.java
@@ -46,42 +46,96 @@ public class C4
 //        return col;
 //        
 //    }
-    
-
+//    
+    public int minimax (int i, Tauler t, int color, boolean seguir){
+        Tauler actual = new Tauler (t);
+        //boolean seguir = false;
+        int col = (int)(t.getMida() * Math.random());
+        if(t.movpossible(i)){
+                //Full attack
+                    actual = new Tauler(t);
+                    actual.afegeix(i, color);
+                    if (actual.solucio(i, color)){
+                        seguir = false;
+                        turno = 0;
+                        col = i;
+                    
+                    }
+                    //Full counter
+                    else{
+                        actual = new Tauler(t);
+                        actual.afegeix(i, color*-1);
+                        if(actual.solucio(i, color*-1)){
+                            col = i;
+                        }
+                    }      
+                }
+        return col;
+    }
     @Override
     public int moviment(Tauler t, int color) {
-        int col;
-        
-        if(turno >= 2){
-            Tauler actual;
-            col = -1;
-            for (int i = 0; i < t.getMida(); i++) {
-                actual = new Tauler(t);
-                actual.afegeix(i, color);
-                if (actual.solucio(i, color)){
-                    col = i;
-                }
+        //color 1 = rojo
+        //color -1 = azul
+        //comprueba primer turno
+        if(color == 1){
+            boolean acaba = false;
+            for(int i = 0; i < t.getMida() && !acaba; ++i){
+               if(t.getColor(0, i) != 0){
+                   acaba = true;
+               }
             }
-            if (col == -1){
-                
-                for (int i = 0; i < t.getMida(); i++) {
-                    actual = new Tauler(t);
-                    actual.afegeix(i, color*-1);
-                    if (actual.solucio(i, color*-1)){
-                        col = i;
+            if(!acaba){
+                turno = 0;
+            }
+        }
+        else{
+            int acaba = 0;
+            for(int i = 0; i < 2 && acaba <= 1; ++i){
+                for(int j = 0; j < t.getMida() && acaba <= 1; ++j){
+                    if(t.getColor(0, i) != 0){
+                    ++acaba;
                     }
                 }
-                if (col == -1){
-                    col = (int)(t.getMida() * Math.random());
-                }
-            }  
+            }
+            if(acaba <= 1){
+                turno = 0;
+            }
+        }
+        
+        int col;
+        boolean seguir = true;
+        if(turno >= 2){
+            Tauler actual = new Tauler (t);
+            col = (int)(t.getMida() * Math.random());
+            for (int i = 0; i < t.getMida() && seguir; i++) {
+                col = minimax (i, t, color, seguir);
+//                if(t.movpossible(i)){
+//                //Full attack
+//                    actual = new Tauler(t);
+//                    actual.afegeix(i, color);
+//                    if (actual.solucio(i, color)){
+//                        seguir = false;
+//                        turno = 0;
+//                        col = i;
+//                    
+//                    }
+//                    //Full counter
+//                    else{
+//                        actual = new Tauler(t);
+//                        actual.afegeix(i, color*-1);
+//                        if(actual.solucio(i, color*-1)){
+//                            col = i;
+//                        }
+//                    }      
+//                }
+            } 
         }
         else if (turno == 1){
-            col = t.getMida()/2;
+            col = t.getMida()/2 - 1;
             ++turno;
         }
         else{
-            col = t.getMida()/2 - 1;
+            col = t.getMida()/2;
             ++turno;
         }
         estado_anterior = new Tauler(t);
