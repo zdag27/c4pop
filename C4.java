545836d577dbd9fbd40e9144f09e7637e68d5a0e
zdag27 <50047402+zdag27@users.noreply.github.com>
2021-11-18 00:21:12 +0100

    Update C4.java

--- C4.java
+++ C4.java
@@ -9,7 +9,7 @@ package edu.epsevg.prop.lab.c4;
  *
  * @author clast
  */
-public class c4 
+public class C4 
         implements Jugador, IAuto{
     
     private String nombre;
@@ -19,7 +19,7 @@ public class c4
         return nombre;
     }
     
-    public c4 (){
+    public C4 (){
         nombre = "C4";
         turno = 0;
     }
@@ -48,15 +48,144 @@ public class c4
     
     public int h(Tauler asme, Tauler asenemy, int color, int col){
         int heuristica = 0;
-        /**///asme.pintaTaulerALaConsola();
-        Tauler enemy=new Tauler(asenemy);
-        enemy.afegeix(col, color*-1);
-        if(enemy.solucio(col, color*-1)){
-            heuristica=5;
-        }else if(asme.solucio(col, color)){
-            heuristica=4;
+        int da=0;
+        int de=0;
+        int h=0;
+        int v=0;
+        int dae=0;
+        int dee=0;
+        int he=0;
+        int ve=0;
+        int maxdame=0;
+        int maxdeme=0;
+        int maxhme=0;
+        int maxvme=0;
+        int maxdahe=0;
+        int maxdehe=0;
+        int maxhhe=0;
+        int maxvhe=0;  
+        for (int i = 0; i < asme.getMida(); i++) {
+            for (int j = 0; j < asme.getMida(); j++) {
+                if(asme.getColor(i, j)==color){
+                    if(i<asme.getMida()-1){
+                        if(asme.getColor(i+1, j)==asme.getColor(i, j) ){
+                            v+=1;
+                        }else{
+                            if(asme.getColor(i+1, j)!=0){
+                                if(maxvme<v){
+                                    maxvme=v;
+                                }
+                                v=0;
+                            }
+                        }
+                    }
+                    if(j<asme.getMida()-1){
+                        if(asme.getColor(i, j+1)==asme.getColor(i, j) ){
+                            h+=1;
+                        }else{
+                            if(asme.getColor(i, j+1)!=0){
+                                if(maxhme<h){
+                                    maxhme=h;
+                                }
+                                h=0;
+                            }
+                        }
+                    }
+                    if(i<asme.getMida()-1 && j<asme.getMida()-1){
+                        if(asme.getColor(i+1, j+1)==asme.getColor(i, j) ){
+                            da+=1;
+                        }else{
+                            if(asme.getColor(i+1, j+1)!=0){
+                                if(maxdame<da){
+                                    maxdame=da;
+                                }
+                                da=0;
+                            }
+                        }
+                    }
+                    if(i>0 && j>0){
+                        if(asme.getColor(i-1, j-1)==asme.getColor(i, j) ){
+                            de+=1;
+                        }else{
+                            if(asme.getColor(i-1, j-1)!=0){
+                                if(maxdeme<de){
+                                    maxdeme=de;
+                                }
+                                de=0;
+                            }
+                        }
+                    }
+                }
+                else if(asme.getColor(i, j)==color*-1){
+                    if(i<asme.getMida()-1){
+                        if(asme.getColor(i+1, j)==asme.getColor(i, j) ){
+                            ve+=1;
+                        }else{
+                            if(asme.getColor(i+1, j)!=0){
+                                if(maxvhe<ve){
+                                    maxvhe=ve;
+                                }
+                                ve=0;
+                            }
+                        }  
+                    }
+                    if(j<asme.getMida()-1){
+                        if(asme.getColor(i, j+1)==asme.getColor(i, j) ){
+                            he+=1;
+                        }else{
+                            if(asme.getColor(i, j+1)!=0){
+                                if(maxhhe<he){
+                                    maxhhe=he;
+                                }
+                                he=0;
+                                }
+                        }
+                    }
+                    if(i<asme.getMida()-1 && j<asme.getMida()-1){
+                        if(asme.getColor(i+1, j+1)==asme.getColor(i, j) ){
+                            dae+=1;
+                        }else{
+                            if(asme.getColor(i+1, j+1)!=0){
+                                if(maxdahe<dae){
+                                    maxdahe=dae;
+                                }
+                                dae=0;
+                            }
+                        }
+                    }
+                    if(i>0 && j>0){
+                        if(asme.getColor(i-1, j-1)==asme.getColor(i, j) ){
+                            dee+=1;
+                        }else{
+                            if(asme.getColor(i-1, j-1)!=0){
+                                if(maxdehe<dee){
+                                    maxdehe=dee;
+                                }
+                                dee=0;
+                            }
+                        }
+                    }
+                }
+            } 
         }
-            return heuristica;
+        
+        heuristica=(((maxdame+maxdeme+maxhme+maxvme) )) - (( 2*(maxdahe+maxdehe+maxhhe+maxvhe) ));
+        if(asme.solucio(col, color)){
+            heuristica = 100;
+        }else{
+            Tauler aux = new Tauler(asenemy);
+            aux.afegeix(col, color*-1);
+            if(aux.solucio(col, color*-1)){
+                heuristica = 100;
+            }else if(asme.movpossible(col)){
+                Tauler aux2 = new Tauler(asme);
+                aux2.afegeix(col, color*-1);
+                if(aux2.solucio(col, color*-1)){
+                    heuristica = -100;
+                }
+            }
+        }
+        return heuristica;
     }
     
     public int minimax2 (int profundidad, Tauler t, int color, int True_Color, int col){ 
@@ -128,8 +257,9 @@ public class c4
        int heuristica_aux=-999999;
        int heuristica = -999999;
        String aux="";
+       String betters="";
        int col = (int)(t.getMida() * Math.random());
-       boolean be_aleatorian=((int)(t.getMida() * Math.random()))%2 == 0;
+       //boolean be_aleatorian=((int)(t.getMida() * Math.random()))%2 == 0;
        for(int i = 0; i < t.getMida(); ++i){
            if(t.movpossible(i)){
                 heuristica_aux = minimax2(profundidad-1,t , color, color, i);
@@ -139,11 +269,20 @@ public class c4
                 aux+="."+heuristica_aux;
                 if(heuristica_aux > heuristica){
                     heuristica = heuristica_aux;
+                    betters="";
+                    betters="."+col;
                     col = i;
+                }
+                if(heuristica_aux == heuristica){
+                    betters="."+col;
                 }
             }
         }
-       //System.out.println(aux);
+       String Rockstars[] = betters.split(".");
+       int auximiliano=(int)((Rockstars.length-1) * Math.random());
+       System.out.println(betters);
+       System.out.println(auximiliano);
+       System.out.println(aux);
       //System.out.println("->>>>>>>>>>>>>>>>>>>>>"+col);
       return col;
    }
@@ -156,13 +295,14 @@ public class c4
         primerturno(t,color);
         int col;
         if(turno >= 2){
-            col=choosemove(t,color,4);
+            col=choosemove(t,color,8);
         }else{
             col=primeras2jugadas(t,color);
         }
         return col;
     }
 }
+
         /*
             int heuristica = 0;
             Tauler auxi = new Tauler(t);
